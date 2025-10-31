10e9a8e4ff0f4396616a7479118ff4939de61f51
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2025-10-31 14:58:46 -0400

    feat: add information section

--- apps/web/src/components/Information/index.tsx
+++ apps/web/src/components/Information/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@tanstack/react-router";
 import { Section } from "@/ui/Section";
 import { SectionHeader } from "@/ui/Section/Header";
 
@@ -6,8 +7,43 @@ export const Information = () => {
 		<Section>
 			<SectionHeader color="red">
 				<div className="flex h-full w-full justify-between">
-					<div className="flex items-center gap-3">first</div>
-					<div className="flex items-center gap-3">second</div>
+					<div className="flex items-center gap-4">
+						<a
+							href="https://discord.gg/mdUhhmRmRK"
+							target="_blank"
+							className="flex flex-row items-center gap-1 font-verdana text-white text-xxs hover:underline"
+							rel="noopener"
+						>
+							<img src="/assets/icons/16/icon-discord.png" alt="discord" />
+							<span>Join discord</span>
+						</a>
+						<a
+							href="/#instagram"
+							target="_blank"
+							className="flex flex-row items-center gap-1 font-verdana text-white text-xxs hover:underline"
+							rel="noopener"
+						>
+							<img src="/assets/icons/16/icon-instagram.png" alt="instagram" />
+							<span>Instagram</span>
+						</a>
+						<Link
+							to="/"
+							className="hidden flex-row items-center gap-1 font-verdana text-white text-xxs hover:underline lg:flex"
+						>
+							<img src="/assets/icons/16/icon-download.png" alt="download" />
+							<span>Download</span>
+						</Link>
+					</div>
+					<Link
+						to="/"
+						className="flex flex-row items-center gap-2 font-verdana text-white text-xxs hover:underline"
+					>
+						<img
+							src="/assets/icons/global/icon-players-online.png"
+							alt="players online"
+						/>
+						<span>1245 Players online</span>
+					</Link>
 				</div>
 			</SectionHeader>
 		</Section>
