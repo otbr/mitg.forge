fbd57541dd5fc93bed36d5ed95f8973375691406
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2025-11-10 21:12:44 -0400

    chore: remove test in account details

--- apps/api/src/application/usecases/account/details/index.ts
+++ apps/api/src/application/usecases/account/details/index.ts
@@ -1,7 +1,5 @@
-import { renderTemplate } from "@miforge/email";
 import { inject, injectable } from "tsyringe";
 import type { AccountsService } from "@/application/services";
-import type { Mailer } from "@/domain/modules/clients";
 import type { Metadata } from "@/domain/modules/metadata";
 import { TOKENS } from "@/infra/di/tokens";
 import type { UseCase } from "@/shared/interfaces/usecase";
@@ -18,22 +16,10 @@ export class AccountDetailsBySessionUseCase
 		@inject(TOKENS.AccountsService)
 		private readonly accountsService: AccountsService,
 		@inject(TOKENS.Metadata) private readonly metadata: Metadata,
-		@inject(TOKENS.Mailer) private readonly mailer: Mailer,
 	) {}
 
 	async execute(): Promise<AccountDetailsContractOutput> {
 		const session = this.metadata.session();
-
-		const html = await renderTemplate("RecoveryKey", {
-			code: "SOME-RECOVERY-KEY",
-			user: session.email,
-		});
-
-		this.mailer.sendMail({
-			to: "gui.fontes.amorim@gmail.com",
-			subject: "Your account details were accessed",
-			html,
-		});
 
 		return this.accountsService.details(session.email);
 	}
