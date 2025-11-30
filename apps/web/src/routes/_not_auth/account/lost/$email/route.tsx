69ebfb59b93b2cdf83f753daab065ae3971853d4
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2025-11-30 14:09:22 -0400

    fix: large bundle using zod in route. and add bundle analyzer

--- apps/web/src/routes/_not_auth/account/lost/$email/route.tsx
+++ apps/web/src/routes/_not_auth/account/lost/$email/route.tsx
@@ -1,27 +1,18 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { toast } from "sonner";
-import z from "zod";
 import { api } from "@/sdk/lib/api/factory";
 
 export const Route = createFileRoute("/_not_auth/account/lost/$email")({
 	loader: async ({ params }) => {
 		const { email } = params;
 
-		const parseResult = await z.email().safeParseAsync(email);
-
-		if (!parseResult.success) {
-			toast.error("Invalid email");
-
-			throw redirect({
-				to: "/account/lost",
-			});
-		}
-
 		await api.client.miforge.lost
 			.findByEmail({
-				email: parseResult.data,
+				email: email,
 			})
 			.catch(() => {
+				toast.error("Email not found");
+
 				throw redirect({
 					to: "/account/lost",
 				});
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import tanstackRouter from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -9,10 +10,32 @@ export default defineConfig({
 		tanstackRouter({ target: "react", autoCodeSplitting: true }),
 		react(),
 		tailwindcss(),
+		visualizer({
+			filename: "bundle-analysis.html",
+			gzipSize: true,
+			brotliSize: true,
+		}),
 	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					form: ["react-hook-form"],
+					react: ["react", "react-dom"],
+					tanstack: [
+						"@tanstack/react-router",
+						"@tanstack/react-query",
+						"@tanstack/react-router-devtools",
+						"@tanstack/react-query-devtools",
+					],
+					zod: ["zod"],
+				},
+			},
 		},
 	},
 });
